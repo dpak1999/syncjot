f9678e060b98fec9c508ad0ea939313b7c0497ec
Deepak <deepakkumardash313@gmail.com>
2023-11-19 01:14:34 +0530

    add : workspace schema

---
+++ src/lib/supabase/schema.ts
@@ -0,0 +1,16 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const workspaces = pgTable("workspaces", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("createdAt", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  workspaceOwner: uuid("workspace_owner").notNull(),
+  title: text("title").notNull(),
+  iconId: text("icon_id").notNull(),
+  data: text("data"),
+  logo: text("logo"),
+  inTrash: text("in_trash"),
+  bannerUrl: text("banner_url"),
+});
