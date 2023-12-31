e9884abb13ce7a5b5d6f11e33cd617e733cffa6a
deepak <deepakkumardash313@gmail.com>
2023-12-31 20:49:44 +0530

    added create workspace ui & api

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,6 +1,42 @@
+import DashboardSetup from "@/components/dashboard-setup/DashboardSetup";
+import db from "@/lib/supabase/db";
+import { getUserSubscriptionStatus } from "@/lib/supabase/queries";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return;
+  }
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
+
+  const { data: subscription, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id);
+
+  if (!workspace) {
+    return (
+      <div className="h-screen bg-background w-screen flex justify-center items-center">
+        <DashboardSetup
+          user={user}
+          subscription={subscription}
+        ></DashboardSetup>
+      </div>
+    );
+  }
+
+  redirect(`/dashboard/${workspace.id}`);
+
   return <div>DashboardPage</div>;
 };
 
